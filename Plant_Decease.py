0e46e837a1231d4247c93d690ee9d913025f69a1
ky042274-web <ky042274@gmail.com>
2026-06-01 16:02:28 +0530

    Update Plant_Decease.py

--- Plant_Decease.py
+++ Plant_Decease.py
@@ -22,7 +22,7 @@ def get_base64(file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-img = get_base64("background.jpg")
+img = get_base64("background.png")
 
 page_bg = f"""
 <style>
